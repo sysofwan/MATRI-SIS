37a43749c6432fac8d38d525436986723c08f5ae
Sayyid Sofwan <sysofwan@umich.edu>
2014-08-17 15:20:00 +0800

    inital commit

---
+++ ng-app/app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name matriSisApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the matriSisApp
+ */
+angular.module('matriSisApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ ng-app/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name matriSisApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the matriSisApp
+ */
+angular.module('matriSisApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
